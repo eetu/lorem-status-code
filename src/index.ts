87fafb724ed89eb3606c7d9b8a2ddfc1b6878099
Eetu Sutinen <eetu.sutinen@gmail.com>
2019-02-19 20:32:27 +0200

    Add some logging

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import * as http from 'http';
 
 import { getRandomStatusCode } from './error-codes';
+import logger from './util/logger';
 
 http
   .createServer((req, res) => {
-    res.writeHead(getRandomStatusCode());
+    const statusCode = getRandomStatusCode();
+    logger.info(`${req.method} ${req.url} ${JSON.stringify(req.headers)} ${statusCode}`);
+    res.writeHead(statusCode);
     res.end();
   })
   .listen(process.env.PORT || 3000);
---
+++ src/util/logger.ts
@@ -0,0 +1,12 @@
+import { createLogger, format, transports } from 'winston';
+
+const level = process.env.LOGGER_LEVEL;
+const isProd = process.env.NODE_ENV === 'production';
+
+const logger = createLogger({
+  format: format.combine(format.colorize({ all: !isProd }), format.simple()),
+  level,
+  transports: [new transports.Console()],
+});
+
+export default logger;
